d6f29c44d4d956978f6fe1bb2613f5aaf82d310f
Ivan <ivanpiccioni.work@gmail.com>
2023-12-06 17:20:57 +0100

    Exercise esecution

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Rettangolo rettangolo = new Rettangolo(5.0, 3.0);
+        Rettangolo rettangolo = new Rettangolo(5, 3);
 
 
         System.out.println("Calcolare l'area del rettangolo:");
